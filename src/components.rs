b64cbeb3d5c4b34140a19205a8974da7f3fd49e0
John Reed <87283488+JohnReedV@users.noreply.github.com>
2023-08-13 18:22:36 -0700

    add jumping

--- src/components.rs
+++ src/components.rs
@@ -4,4 +4,13 @@ use bevy::prelude::*;
 pub struct PlayerCamera {}
 
 #[derive(Component)]
-pub struct Player {}
+pub struct Player {
+    pub jump: Jump
+}
+
+pub struct Jump {
+    pub is_jumping: bool,
+    pub jump_timer: f32,
+    pub jump_y: f32,
+    pub jump_cooldown_timer: f32,
+}
--- src/player/systems.rs
+++ src/player/systems.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
-
-use crate::resources::*;
 use crate::components::*;
+use crate::resources::*;
 
 pub const PLAYER_SPEED: i32 = 1000;
+const JUMP_SCALE: f32 = 1.2;
+const JUMP_DURATION: f32 = 0.5;
+const JUMP_PEAK_HEIGHT: f32 = 5.0 * 32.0;
+const JUMP_HALF_DURATION: f32 = JUMP_DURATION / 2.0;
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -17,7 +20,14 @@ pub fn spawn_player(
                 texture: asset_server.load("sprites/player.png"),
                 ..default()
             },
-            Player {},
+            Player {
+                jump: Jump {
+                is_jumping: false,
+                jump_timer: 0.0,
+                jump_y: 0.0,
+                jump_cooldown_timer: 0.0,
+                }
+            },
         ));
     }
 }
@@ -36,32 +46,70 @@ pub fn despawn_player(
 
 pub fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Transform, With<Player>>,
+    mut player_query: Query<(&mut Transform, &mut Player), With<Player>>,
     mut camera_query: Query<&mut Transform, (With<PlayerCamera>, Without<Player>)>,
     time: Res<Time>,
 ) {
-    if let Ok(mut transform) = player_query.get_single_mut() {
+    if let Ok((mut transform, mut player)) = player_query.get_single_mut() {
         let mut direction = Vec3::ZERO;
 
         if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-            direction += Vec3::new(-1.0, 0.0, 0.0)
+            direction += Vec3::new(-1.0, 0.0, 0.0);
         }
         if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-            direction += Vec3::new(1.0, 0.0, 0.0)
+            direction += Vec3::new(1.0, 0.0, 0.0);
         }
         if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0.0, 1.0, 0.0)
+            direction += Vec3::new(0.0, 1.0, 0.0);
         }
         if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-            direction += Vec3::new(0.0, -1.0, 0.0)
+            direction += Vec3::new(0.0, -1.0, 0.0);
         }
 
         if direction.length() > 0.0 {
-            direction = direction.normalize()
+            direction = direction.normalize();
         }
 
         let move_distance = direction * PLAYER_SPEED as f32 * time.delta_seconds();
         transform.translation += move_distance;
+
+        if keyboard_input.just_pressed(KeyCode::Space) && !player.jump.is_jumping && player.jump.jump_cooldown_timer <= 0.0
+            || keyboard_input.pressed(KeyCode::Space) && !player.jump.is_jumping && player.jump.jump_cooldown_timer <= 0.0
+        {
+            transform.translation.z = 2.1;
+            player.jump.is_jumping = true;
+            transform.scale *= Vec3::splat(JUMP_SCALE);
+            player.jump.jump_y = transform.translation.y;
+            player.jump.jump_cooldown_timer = 0.14;
+        }
+
+        if player.jump.is_jumping {
+            player.jump.jump_timer += time.delta_seconds();
+
+            if player.jump.jump_timer <= JUMP_HALF_DURATION {
+                transform.translation.y = player.jump.jump_y
+                    + (4.0 * JUMP_PEAK_HEIGHT / (JUMP_DURATION * JUMP_DURATION))
+                        * player.jump.jump_timer
+                        * player.jump.jump_timer;
+            } else {
+                transform.translation.y = player.jump.jump_y
+                    + (4.0 * JUMP_PEAK_HEIGHT / (JUMP_DURATION * JUMP_DURATION))
+                        * (player.jump.jump_timer - JUMP_DURATION)
+                        * (player.jump.jump_timer - JUMP_DURATION);
+            }
+
+            if player.jump.jump_timer >= JUMP_DURATION {
+                transform.translation.z = 1.0;
+                player.jump.is_jumping = false;
+                transform.scale /= Vec3::splat(JUMP_SCALE);
+                player.jump.jump_timer = 0.0;
+                transform.translation.y = player.jump.jump_y;
+            }
+        } else {
+            if player.jump.jump_cooldown_timer > 0.0 {
+                player.jump.jump_cooldown_timer -= time.delta_seconds();
+            }
+        }
 
         if let Ok(mut camera_transform) = camera_query.get_single_mut() {
             camera_transform.translation.x = transform.translation.x;
@@ -69,4 +117,3 @@ pub fn player_movement(
         }
     }
 }
-
